fffc10421ac734875f95f520a4e791c05c7e899a
Mark Meyof Kweyui Ngongang <meyofmarksog@gmail.com>
2025-09-03 22:36:30 -0400

    making endpoint public

--- infra/main.tf
+++ infra/main.tf
@@ -49,6 +49,9 @@ module "eks" {
     subnet_ids      = module.vpc.private_subnets
     vpc_id          = module.vpc.vpc_id
     
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true   # just for testing purpose
+
     # access for iam users
     access_entries = {
         for user in var.admin_users : user => {
